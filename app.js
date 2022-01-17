ae542f57fd9263eae3bf047f6c88500d4dfabf68
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-01-17 15:43:37 +0100

    da completare

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let app = new Vue ({
-  el: #app,
+let app = new Vue({
+  el: "#app",
   data: {
-    
+    nome: "",
+    toDoList: [],
+    fatto: "fatto",
+  },
+  methods: {
+    aggiungi: function () {
+      if (!this.nome == "") {
+        this.toDoList.push({ text: this.nome, done: false });
+      }
+
+      this.nome = "";
+    },
+
+    elimina: function (indice) {
+      this.toDoList.splice(indice, 1);
+    },
+
+    isDone: function (indice) {
+      this.toDoList[indice].done = !this.toDoList[indice].done;
+    },
   }
-})
+});
